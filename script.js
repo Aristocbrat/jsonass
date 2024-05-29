31862294d2c586ad036db65a97a39b852cbf415e
Aristocbrat <emmanuelforbez@gmail.com>
2024-05-29 13:36:25 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+
+fetch('https://jsonplaceholder.typicode.com/todos')
+  .then(response => {
+    return response.json();
+  })
+  .then(todos => {
+  
+    const todoOrigin = document.getElementById('todoOrigin');
+
+    todos.forEach(todo => {
+      // Create a div element for each to-do item
+      const todoDiv = document.createElement('div');
+      // Set the text content of the div
+      todoDiv.textContent = `Id: ${todo.id},
+      userId:${todo.userId}
+       Title: ${todo.title},
+        Completed: ${todo.completed}`;
+      // set your conditions and assign them to the appropriate class
+      if (todo.completed) {
+        todoDiv.classList.add('t-item', 'completed');
+      } else {
+        todoDiv.classList.add('t-item', 'uncompleted');
+      }
+      // open new blank page ,
+      todoDiv.addEventListener('click', () => {
+        window.open(``, 'newpage',"width=500,height=500");
+      });
+      todoOrigin.appendChild(todoDiv);
+    });
+  })
+  .catch(error => {
+    console.log( error);
+  });
+
